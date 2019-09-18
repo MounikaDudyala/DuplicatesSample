562c68f428aa89951ef4c07ce36542da1d44e2b3
MounikaDudyala <mounikadudyala1998@gmail.com>
2019-09-18 16:01:59 +0530

    Linked List Sample

---
+++ src/com/linkedlistsample/LinkedListSample.java
@@ -0,0 +1,38 @@
+package com.linkedlistsample;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListSample {
+	public static void main(String args[])
+	{
+		LinkedList<Integer> list1=new LinkedList<>();
+		LinkedList<Integer> list2=new LinkedList<>();
+		list1.addFirst(5);list1.addFirst(9);list1.addFirst(8);
+        list2.addFirst(2);list2.addFirst(6);list2.addFirst(4);
+        LinkedList<Integer> list3=findSum(list1,list2);
+        printList(list3);
+	}
+
+	private static LinkedList<Integer> findSum(LinkedList<Integer> list1, LinkedList<Integer> list2) {
+		int div=0;
+        LinkedList<Integer> list3=new LinkedList<>(); 
+        for(int i=list1.size(),j=list2.size();i>=1||j>=1;i--,j--)
+        {
+        	list3.add((div+list1.get(i-1)+list2.get(j-1))%10);
+        	div=(div+list1.get(i-1)+list2.get(j-1))/10;
+        }
+        if(div!=0) {
+        list3.add(div);
+        }
+		return list3;
+	}
+private static void printList(LinkedList<Integer> list3)
+{
+	Iterator<Integer> iterator=list3.listIterator();
+	while(iterator.hasNext())
+	{
+		System.out.println(iterator.next());
+	}
+}
+}
