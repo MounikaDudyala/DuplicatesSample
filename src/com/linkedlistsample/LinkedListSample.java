d43f878009ee77a6642d1f621dd6ba5eddc9ae80
MounikaDudyala <mounikadudyala1998@gmail.com>
2019-09-27 16:19:46 +0530

    using stacks updated

--- src/com/linkedlistsample/LinkedListSample.java
+++ src/com/linkedlistsample/LinkedListSample.java
@@ -8,13 +8,13 @@ public class LinkedListSample {
 	public static void main(String args[]) {
 		LinkedList<Integer> list1 = new LinkedList<>();
 		LinkedList<Integer> list2 = new LinkedList<>();
+		list1.add(2);
 		list1.add(3);
 		list1.add(4);
-		list1.add(2);
-		list2.add(4);
-		list2.add(6);
-		list2.add(5);
-
+		list1.add(5);
+		list1.add(6);
+		list2.add(2);
+		list2.add(1);
 		try {
 			LinkedList<Integer> list3 = findSum1(list1, list2);
 			printList(list3);
@@ -25,7 +25,6 @@ public class LinkedListSample {
 		} catch (Exception e) {
 			System.out.println("no output");
 		}
-
 	}
 
 	/*
@@ -34,10 +33,15 @@ public class LinkedListSample {
 	 */
 	private static LinkedList<Integer> findSum1(LinkedList<Integer> list1, LinkedList<Integer> list2)
 			throws NullPointerException {
-		if (list1.isEmpty() || list2.isEmpty() || list1.size() != list2.size()) {
+		if (isEmpty(list1) || isEmpty(list2)) {
 			throw new NullPointerException();
-
 		} else {
+			while (list1.size() != list2.size()) {
+				if (list1.size() > list2.size())
+					list2.addFirst(0);
+				else
+					list1.addFirst(0);
+			}
 			int div = 0;
 			LinkedList<Integer> list3 = new LinkedList<>();
 			for (int i = list1.size(), j = list2.size(); i >= 1 || j >= 1; i--, j--) {
@@ -49,7 +53,6 @@ public class LinkedListSample {
 			}
 			return list3;
 		}
-
 	}
 
 	/*
@@ -57,10 +60,15 @@ public class LinkedListSample {
 	 */
 	private static LinkedList<Integer> findSum2(LinkedList<Integer> list1, LinkedList<Integer> list2)
 			throws NullPointerException {
-		if (list1.isEmpty() || list2.isEmpty() || list1.size() != list2.size()) {
+		if (isEmpty(list1) || isEmpty(list2)) {
 			throw new NullPointerException();
-
 		} else {
+			while (list1.size() != list2.size()) {
+				if (list1.size() > list2.size())
+					list2.addFirst(0);
+				else
+					list1.addFirst(0);
+			}
 			LinkedList<Integer> list3 = new LinkedList<>();
 			Stack<Integer> stack1 = new Stack<Integer>();
 			Stack<Integer> stack2 = new Stack<Integer>();
@@ -70,7 +78,6 @@ public class LinkedListSample {
 			}
 			for (int j = 0; j < list2.size(); j++) {
 				stack2.push(list2.get(j));
-
 			}
 			int div = 0;
 			for (int i = 1, j = 1; i <= list1.size() && j <= list2.size(); i++, j++) {
@@ -91,10 +98,15 @@ public class LinkedListSample {
 	 */
 	private static LinkedList<Integer> findSum3(LinkedList<Integer> list1, LinkedList<Integer> list2)
 			throws NullPointerException {
-		if (list1.isEmpty() || list2.isEmpty() || list1.size() != list2.size()) {
+		if (isEmpty(list1) || isEmpty(list2)) {
 			throw new NullPointerException();
-
 		} else {
+			while (list1.size() != list2.size()) {
+				if (list1.size() > list2.size())
+					list2.addFirst(0);
+				else
+					list1.addFirst(0);
+			}
 			LinkedList<Integer> list3 = new LinkedList<>();
 			LinkedList<Integer> list4 = new LinkedList<>();
 			LinkedList<Integer> list5 = new LinkedList<>();
@@ -123,6 +135,14 @@ public class LinkedListSample {
 		}
 	}
 
+	/* method to check list for null or empty */
+	static boolean isEmpty(LinkedList<Integer> list) {
+		if (list == null || list.size() == 0)
+			return true;
+		return false;
+	}
+
+	/* method to print a list */
 	private static void printList(LinkedList<Integer> list3) {
 		Iterator<Integer> iterator = list3.listIterator();
 		while (iterator.hasNext()) {
